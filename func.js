da06bee579e38309186df0036d4f7ecdcfacd470
maxide <maximilian@duvenhorst.net>
2023-04-30 23:26:18 +0200

    first Commit

---
+++ func.js
@@ -0,0 +1,18 @@
+document.querySelectorAll('nav ul li a:not(:only-child)').forEach(element => {
+  element.addEventListener('click', event => {
+    $(anchor).siblings('.nav-dropdown').toggle();
+    // Close one dropdown when selecting another
+    $('.nav-dropdown').not($(anchor).siblings()).hide();
+    event.stopPropagation()
+  })
+})
+
+document.addEventListener('click', () => $('.nav-dropdown').hide())
+
+const navToggle = document.querySelector('#nav-toggle')
+navToggle.addEventListener('click', () => {
+  navToggle.classList.toggle('active')
+  $('nav ul').slideToggle()
+})
+
+
